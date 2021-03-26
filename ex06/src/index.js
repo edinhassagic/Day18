d31f465a1c9dd1a635ee574510e9ffe365b982aa
edin hasagic <edinhassagic@gmail.com>
2021-03-26 10:42:34 +0100

    6

---
+++ ex06/src/index.js
@@ -0,0 +1,10 @@
+let rockStar = new Map([
+    ["artist", "The Rolling Stones"],
+    ["song", "Angie"],
+    ["album", "Goats Head Soup"],
+    ["singer", "Mick Jagger"],
+  ]);
+  let newRock = new Map([...rockStar].filter(([k, v]) => k[0] > "a"));
+  console.log(newRock);
+
+  module.exports = { rockStar, newRock };
